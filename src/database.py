eaf7f6a2e63a43650a4ade85f0fae6f76c0c9cbb
Brunomdd <brunnodiniiz@gmail.com>
2026-09-12 20:17:50 -0300

    feat: adiciona arquivo de banco

---
+++ src/database.py
@@ -0,0 +1,14 @@
+from sqlmodel import SQLModel, create_engine, Session
+sql_name = "banco.db"
+sql_file_name = f"sqlite:///banco.db"
+connect_args = {"check_same_thread":False}
+engine = create_engine(sql_file_name,connect_args=connect_args)
+
+
+def create_and_db():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+

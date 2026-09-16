8f069872b3910ecd40608816bdff32dd3ffc1a83
Brunomdd <brunnodiniiz@gmail.com>
2026-09-16 18:20:57 -0300

    refactor: .

--- src/main.py
+++ src/main.py
@@ -19,12 +19,6 @@ async def lifespan(app: FastAPI):
     create_and_db()
     yield
 
-
-app = FastAPI(
-    lifespan=lifespan,
-    title="API"
-)
-
 SessionDP = Annotated[
     Session,
     Depends(get_session)
@@ -33,10 +27,10 @@ SessionDP = Annotated[
 app = FastAPI(lifespan=lifespan,title='API')
 Sessions = Annotated[Session,Depends(get_session)]
 IdUsuario = Annotated[int,Path(title="ID do item",ge=1)]
-NomeUsuario = Annotated[str,Query(min_length=3,max_length=50)]
+NomeUsuario = Annotated[str,Query(pattern=r"^[^*!#()%^@]+$",min_length=3,max_length=50)]
 
-@app.post("/criar/",tags=['Criar usuário'],response_model=PessoaPublica)
-def criar_usuario(pessoa:CriarPessoa,session:Sessions): 
+@app.post("/usuarios/criar",tags=['Criar usuário'],response_model=PessoaPublica)
+def criar_usuario(pessoa:CriarPessoa,session:SessionDP): 
     statement = select(Pessoa).where(Pessoa.email == pessoa.email)
     pessoa_existente = session.exec(statement).first()
     if pessoa_existente:
@@ -47,53 +41,55 @@ def criar_usuario(pessoa:CriarPessoa,session:Sessions):
     session.refresh(validar)
     return validar
 
-@app.get("/listar/usuarios",tags=['Listar Usuários'],response_model=list[PessoaPublica])
+@app.get("/usuarios/listar",tags=['Listar Usuários'],response_model=list[PessoaPublica])
 def listar_todos(session:SessionDP, 
-            offset:Annotated[int,Query(ge=0)],
+            offset:Annotated[int,Query(ge=0)] = 0,
             limit:Annotated[int,Query(le=100)] = 100):
     pessoas = session.exec(select(Pessoa).offset(offset).limit(limit)).all()
     return pessoas
 
-@app.get("/buscar/nome",response_model=list[PessoaPublica],tags=['Buscar por nome'])
+@app.get("/usuarios/nome",response_model=list[PessoaPublica],tags=['Buscar por nome'])
 def buscar_nome_usuario(session:Sessions,nome:NomeUsuario):
     statement = select(Pessoa).where(Pessoa.nome == nome)
     resultado = session.exec(statement).all()
     return resultado
 
-@app.get("/busca/parcial/nome",response_model=list[PessoaPublica],tags=["Busca parcial por nome"])
-def busca_parcial(session:Sessions,nome:str):
-    statement = select(Pessoa).where(Pessoa.nome.contains(nome))
-    pessoas = session.exec(statement)
-    return pessoas
+@app.get("/usuarios/buscar/parcial",response_model=list[PessoaPublica],tags=["Busca parcial por nome"])
+def busca_parcial(session:SessionDP
+                  ,nome:NomeUsuario,offset:Annotated[int,Query(ge=0)] = 0,
+                  limit:Annotated[int,Query(le=100)]=100):
+    statement = select(Pessoa).where(Pessoa.nome.contains(nome)).offset(offset).limit(limit)
+    resultado = session.exec(statement).all()
+    return resultado
 
-@app.get("/buscar/usuario/{id_usuario}",tags=['Buscar usuário'],response_model=PessoaPublica)
+@app.get("/usuarios/{id_usuario}",tags=['Buscar usuário'],response_model=PessoaPublica)
 def buscar_id_usuario(id_usuario:IdUsuario,
-            session:Sessions):
+            session:SessionDP):
     get_usuario = session.get(Pessoa,id_usuario)
     if not get_usuario:
         raise HTTPException(status_code=404,detail='Usuário não encontrado')
     return get_usuario
 
-@app.patch("/atualizar/{buscar_id}",response_model=PessoaPublica,tags=['Atualizar campos do usuário'])
+@app.patch("/usuarios/{buscar_id}",response_model=PessoaPublica,tags=['Atualizar campos do usuário'])
 def atualizar_user(buscar_id:IdUsuario,
              session:Sessions,pessoa:PessoaAtualizar):
     buscar_usuario = session.get(Pessoa,buscar_id)
     if not buscar_usuario:
         raise HTTPException(status_code=404,detail="Usuário não encontrado")
-
     pessoa_db = pessoa.model_dump(exclude_unset=True)
-    statement = select(Pessoa).where(Pessoa.email== pessoa.email,Pessoa.id != buscar_usuario.id)
-    pessoa_existente = session.exec(statement).first()
-    if pessoa_existente:
-        raise HTTPException(status_code=409,detail="Email em uso!")
+    if "email" in pessoa_db and pessoa.email is not None:
+        statement = select(Pessoa).where(Pessoa.email== pessoa.email,Pessoa.id != buscar_usuario.id)
+        pessoa_existente = session.exec(statement).first()
+        if pessoa_existente:
+            raise HTTPException(status_code=409,detail="Email em uso!")
     buscar_usuario.sqlmodel_update(pessoa_db)
     session.add(buscar_usuario)
     session.commit()
     session.refresh(buscar_usuario)
     return buscar_usuario
 
-@app.delete("/deletar/{buscar_id}",tags=['Remover usuário'],response_model=Msg)
-def deletar_user(buscar_id:IdUsuario,session:Sessions):
+@app.delete("/usuarios/{buscar_id}",tags=['Remover usuário'],response_model=Msg)
+def deletar_user(buscar_id:IdUsuario,session:SessionDP):
     buscar_usuario = session.get(Pessoa,buscar_id)
     if not buscar_usuario:
         raise HTTPException(status_code=404,detail='Usuário não encontrado')
